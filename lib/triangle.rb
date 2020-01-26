d9410e50d093fe936900694a970133592a0bd2b1
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-26 21:09:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,11 +2,12 @@ require "pry"
 class Triangle
   attr_accessor :side_a, :side_b, :side_c,
   :equilateral, :isosceles, :scalene
-    @@all_sides = []
+    
   def initialize(side_a, side_b, side_c)
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c
+    
   end
   
   def kind 
