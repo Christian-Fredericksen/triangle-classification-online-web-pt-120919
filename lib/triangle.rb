fda43d9b7bc0b6f4a934d0f752301b51356c880e
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-26 19:49:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,10 @@ class Triangle
   end
   
   def kind 
-    
+    if side_a == side_b && side_b == side_c
+      return :equilateral
+    elsif side_a == side_b || side_b == side_c
+      return :isosceles
   end
   
   
